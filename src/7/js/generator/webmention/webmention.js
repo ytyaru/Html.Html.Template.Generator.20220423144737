5228cfc0ed4da4adc35ab5601a3c3a394d1770f0
ytyaru <ytyaru@outlook.jp>
2022-04-30 18:57:30 +0900

    7 リファクタリング（WebMensionのgenerate()をasync記法にした）

--- src/7/js/generator/webmention/webmention.js
+++ src/7/js/generator/webmention/webmention.js
@@ -7,6 +7,16 @@ class WebMention {
     get Me() { return `<link rel="me" href="https://twitter.com/${this.me}">`; }
     get WebMension() { return `<link rel="webmention" href="https://webmention.io/${this.domain}/webmention">`; }
     get Pingback() { return `<link rel="pingback" href="https://webmention.io/${this.domain}/xmlrpc">`; }
+    async generate() {
+        const res = await fetch(`https://webmention.io/api/mentions.jf2?domain=${this.domain}&token=${this.token}`)
+        const mension = await res.json();
+        // ここでJSONを使ってHTMLを動的生成する。だれかどんなコメントをしたか。
+        return this.#generateMension(resJson);
+    }
+    #generateMension(json) {
+        return `WebMensionの情報から動的HTMLを生成する。`
+    }
+    /*
     generate() {
         fetch(`https://webmention.io/api/mentions.jf2?domain=${this.domain}&token=${this.token}`, {
             method: 'GET'    
@@ -23,7 +33,5 @@ class WebMention {
             console.error(error);
         })
     }
-    #generateMension(json) {
-        return `WebMensionの情報から動的HTMLを生成する。`
-    }
+    */
 }
